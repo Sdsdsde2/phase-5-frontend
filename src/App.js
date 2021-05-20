eb6983c22a9fe6dde2af7382c04ad137928f631a
Sdsdsde2 <imspammails31@gmail.com>
2021-05-20 12:06:07 -0500

    main router changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from './components/Home';
+import Dash from './components/Dash';
 
 function App() {
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    let newUser = {
+      name: evt.target[0].value,
+      username: evt.target[1].value,
+      password: evt.target[2].value,
+      credits: 200
+    }
+
+    fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newUser)
+    })
+    // .then(resp => resp.json())
+    // .then(data => {
+    //   localStorage.setItem("token", data.jwt)
+    // })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/dash"} component={Dash} />
+          </Switch>
+        </BrowserRouter>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" />
+          <input type="text" name="username" />
+          <input type="password" name="password" />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
---
+++ src/components/Dash.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dash = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
+
+export default Dash
